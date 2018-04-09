ad9bc6144ead9776cd188a09f6db6630189624a1
L-eeX <1035868977@qq.com>
2018-04-09 09:41:25 +0800

    test

--- main.ts
+++ main.ts
@@ -1,62 +1,78 @@
+/*！
+ * @file pxt-motor/main.ts
+ * @brief DFRobot's microbit motor drive makecode library.
+ * @n [Get the module here](http://www.dfrobot.com.cn/goods-1577.html)
+ * @n This is the microbit special motor drive library, which realizes control 
+ *    of the eight-channel steering gear, two-step motor and four-way dc motor.
+ *
+ * @copyright	[DFRobot](http://www.dfrobot.com), 2016
+ * @copyright	GNU Lesser General Public License
+ *
+ * @author [email](1035868977@qq.com)
+ * @version  V1.0
+ * @date  2018-03-20
+ */
+
 /**
  *This is DFRobot:motor user motor and steering control function.
  */
 //% weight=10 color=#DF6721 icon="\uf013" block="DF-Driver"
 namespace motor {
-    const PCA9634_ADDR = 0x00
-    const PCA9634_REG_MODE1	=	0x00		// Mode register 1
-    const PCA9634_REG_MODE2	=	0x01		// Mode register 2
-    const PCA9634_REG_PWM0	=	0x02		// brightness control LED0
-    const PCA9634_REG_PWM1	=	0x03		// brightness control LED1
-    const PCA9634_REG_PWM2	=	0x04		// brightness control LED2
-    const PCA9634_REG_PWM3	=	0x05		// brightness control LED3
-    const PCA9634_REG_PWM4	=	0x06		// brightness control LED4
-    const PCA9634_REG_PWM5	=	0x07		// brightness control LED5
-    const PCA9634_REG_PWM6	=	0x08		// brightness control LED6
-    const PCA9634_REG_PWM7	=	0x09		// brightness control LED7
-    const PCA9634_REG_GRPPWM	=	0x0A		// group duty cycle
-    const PCA9634_REG_GRPFREQ	=	0x0B		// group frequency
-    const PCA9634_REG_LEDOUT0	=	0x0C		// LED output state 0
-    const PCA9634_REG_LEDOUT1	=	0x0D		// LED output state 1
-    const PCA9634_REG_SUBADR1	=	0x0E		// I2C-bus subaddress 1
-    const PCA9634_REG_SUBADR2	=	0x0F		// I2C-bus subaddress 2
-    const PCA9634_REG_SUBADR3	=	0x10		// I2C-bus subaddress 3
-    const PCA9634_REG_ALLCALLADR =	0x11		// LED All I2C-bus address
-    
-    const PCA9634_ALL_LED_TO_OFF	=			0x00
-    const PCA9634_ALL_LED_TO_ON		=		0x55
-    const PCA9634_ALL_LED_TO_PWM	=			0xAA
-    const PCA9634_ALL_LED_TO_GRPPWM	=		0xFF
-    
-    const ADDRESS_HI				=			0x12
-    const ADDRESS_LO				=			0x34
-    
-    const PCA9634_ERROR_INVALID_LED     =      -1
+    const PCA9685_ADDRESS = 0x40
+    const MODE1 = 0x00
+    const MODE2 = 0x01
+    const SUBADR1 = 0x02
+    const SUBADR2 = 0x03
+    const SUBADR3 = 0x04
+    const PRESCALE = 0xFE
+    const LED0_ON_L = 0x06
+    const LED0_ON_H = 0x07
+    const LED0_OFF_L = 0x08
+    const LED0_OFF_H = 0x09
+    const ALL_LED_ON_L = 0xFA
+    const ALL_LED_ON_H = 0xFB
+    const ALL_LED_OFF_L = 0xFC
+    const ALL_LED_OFF_H = 0xFD
 
-    /**
-     * The user can choose the step motor model.
-     */
-    export enum Stepper { 
-        //% block="42"
-        Ste1 = 1,
-        //% block="28"
-        Ste2 = 2
-    }
+    const STP_CHA_L = 2047
+    const STP_CHA_H = 4095
+
+    const STP_CHB_L = 1
+    const STP_CHB_H = 2047
+
+    const STP_CHC_L = 1023
+    const STP_CHC_H = 3071
+
+    const STP_CHD_L = 3071
+    const STP_CHD_H = 1023
+
+
+    const BYG_CHA_L = 3071
+    const BYG_CHA_H = 1023
+
+    const BYG_CHB_L = 1023
+    const BYG_CHB_H = 3071
+
+    const BYG_CHC_L = 4095
+    const BYG_CHC_H = 2047
+
+    const BYG_CHD_L = 2047
+    const BYG_CHD_H = 4095
 
     /**
      * The user can select the 8 steering gear controller.
      */
     export enum Servos {
-        S1 = 0x03,
-        S2 = 0x02,
+        S1 = 0x04,
+        S2 = 0x05,
     }
 
     /**
      * The user selects the 4-way dc motor.
      */
     export enum Motors {
-        M1 = 0x1,
-        M2 = 0x2
+        M1 = 0x2,
+        M2 = 0x3
     }
 
     /**
@@ -69,35 +85,61 @@ namespace motor {
         CCW = -1,
     }
 
-    /**
-     * The user can select a two-path stepper motor controller.
-     */
-    export enum Steppers {
-        M1_M2 = 0x1,
-        M3_M4 = 0x2
-    }
-
     let initialized = false
 
-    function write_reg(reg: number, value: number) {
+    function i2cWrite(addr: number, reg: number, value: number) {
         let buf = pins.createBuffer(2)
         buf[0] = reg
         buf[1] = value
-        pins.i2cWriteBuffer(PCA9634_ADDR, buf)
+        pins.i2cWriteBuffer(addr, buf)
     }
 
-    function initPCA9634(): void {
-        write_reg( PCA9634_REG_MODE1, 0x00);
-        write_reg( PCA9634_REG_MODE2, 0x16);
-        write_reg( PCA9634_REG_LEDOUT0, PCA9634_ALL_LED_TO_PWM);
-        write_reg( PCA9634_REG_LEDOUT1, PCA9634_ALL_LED_TO_PWM);
+    function i2cCmd(addr: number, value: number) {
+        let buf = pins.createBuffer(1)
+        buf[0] = value
+        pins.i2cWriteBuffer(addr, buf)
+    }
+
+    function i2cRead(addr: number, reg: number) {
+        pins.i2cWriteNumber(addr, reg, NumberFormat.UInt8BE);
+        let val = pins.i2cReadNumber(addr, NumberFormat.UInt8BE);
+        return val;
+    }
+
+    function initPCA9685(): void {
+        i2cWrite(PCA9685_ADDRESS, MODE1, 0x00)
+        setFreq(50);
         initialized = true
     }
 
-    function set_brightness(led: number, duty_cycle: number): void { 
-        write_reg(PCA9634_REG_PWM0 + led, duty_cycle)
+    function setFreq(freq: number): void {
+        // Constrain the frequency
+        let prescaleval = 25000000;
+        prescaleval /= 4096;
+        prescaleval /= freq;
+        prescaleval -= 1;
+        let prescale = prescaleval;//Math.floor(prescaleval + 0.5);
+        let oldmode = i2cRead(PCA9685_ADDRESS, MODE1);
+        let newmode = (oldmode & 0x7F) | 0x10; // sleep
+        i2cWrite(PCA9685_ADDRESS, MODE1, newmode); // go to sleep
+        i2cWrite(PCA9685_ADDRESS, PRESCALE, prescale); // set the prescaler
+        i2cWrite(PCA9685_ADDRESS, MODE1, oldmode);
+        control.waitMicros(5000);
+        i2cWrite(PCA9685_ADDRESS, MODE1, oldmode | 0xa1);
     }
 
+    function setPwm(channel: number, on: number, off: number): void {
+        if (channel < 0 || channel > 15)
+            return;
+
+        let buf = pins.createBuffer(5);
+        buf[0] = LED0_ON_L + 4 * channel;
+        buf[1] = on & 0xff;
+        buf[2] = (on >> 8) & 0xff;
+        buf[3] = off & 0xff;
+        buf[4] = (off >> 8) & 0xff;
+        pins.i2cWriteBuffer(PCA9685_ADDRESS, buf);
+    }
 
     /**
 	 * Steering gear control function.
@@ -110,14 +152,13 @@ namespace motor {
     //% index.fieldEditor="gridpicker" index.fieldOptions.columns=4
     export function servo(index: Servos, degree: number): void {
         if (!initialized) {
-            initPCA9634()
+            initPCA9685()
         }
         // 100hz
         let v_us = (degree * 10 + 600) // 0.6ms ~ 2.4ms
-        let value = v_us * 255 / (1000000 / 100)
-        //setPwm(index + 7, 0, value)
+        let value = v_us * 4095 / (1000000 / 100)
+        setPwm(index + 7, 0, value)
     }
-
 
     /**
 	 * Execute a motor
@@ -126,34 +167,52 @@ namespace motor {
     */
     //% weight=90
     //% blockId=motor_MotorRun block="Motor|%index|dir|%Dir|speed|%speed"
-    //% speed.min=0 speed.max=255
+    //% speed.min=0 speed.max=256
     //% index.fieldEditor="gridpicker" index.fieldOptions.columns=2
     //% direction.fieldEditor="gridpicker" direction.fieldOptions.columns=2
-    export function MotorRun(index: Motors, direction: Dir, speed: number): void {
+    export function MotorRun(index: Motors, direction:Dir, speed: number): void {
         if (!initialized) {
-            initPCA9634()
+            initPCA9685()
         }
-        switch (index) { 
-            case Motors.M1: { 
-                if (direction == Dir.CW) {
-                    set_brightness(4, speed)
-                    set_brightness(5, 0)
-                } else if (direction == Dir.CCW) { 
-                    set_brightness(4, 0)
-                    set_brightness(5, speed)
-                }
-            } break;
-            case Motors.M2: {
-                if (direction == Dir.CW) {
-                    set_brightness(6, speed)
-                    set_brightness(7, 0)
-                } else if (direction == Dir.CCW) { 
-                    set_brightness(6, 0)
-                    set_brightness(7, speed)
-                }
-            } break;
+        speed = speed * 16 * direction; // map 255 to 4096
+        if (speed >= 4096) {
+            speed = 4095
         }
-
+        if (speed <= -4096) {
+            speed = -4095
+        }
+        if (index > 4 || index <= 0)
+            return
+        let pn = (4-index) * 2
+        let pp = (4-index) * 2 + 1 
+        if (speed >= 0) {
+            setPwm(pp, 0, speed)
+            setPwm(pn, 0, 0)
+        } else {
+            setPwm(pp, 0, 0)
+            setPwm(pn, 0, -speed)
+        }
     }
 
+    /**
+	 * Stop the dc motor.
+    */
+    //% weight=20
+    //% blockId=motor_motorStop block="Motor stop|%index"
+    //% index.fieldEditor="gridpicker" index.fieldOptions.columns=2 
+    export function motorStop(index: Motors) {
+        setPwm((4 - index) * 2, 0, 0);
+        setPwm((4 - index) * 2 + 1, 0, 0);
+    }
+
+    /**
+	 * Stop all motors
+    */
+    //% weight=10
+    //% blockId=motor_motorStopAll block="Motor Stop All"
+    export function motorStopAll(): void {
+        for (let idx = 1; idx <= 4; idx++) {
+            motorStop(idx);
+        }
+    }
 }
